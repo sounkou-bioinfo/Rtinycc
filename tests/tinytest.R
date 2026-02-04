7748d12f4236762fb253869f8566825c241ae24a
sounkou-bioinfo <sounkoutoure@gmail.com>
2026-02-04 17:23:57 +0100

    tests: run the tests since the segfault issue for callbacks is gone

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,12 +1,4 @@
 library(Rtinycc)
 if (requireNamespace("tinytest", quietly = TRUE)) {
-  # Skip tests during R CMD check to avoid segfault in cleanup
-  # Tests can be run manually with: make test
-  if (!nzchar(Sys.getenv("_R_CHECK_PACKAGE_NAME_"))) {
-    tinytest::test_package("Rtinycc")
-  } else {
-    message(
-      "Skipping tests during R CMD check - run 'make test' to execute tests manually"
-    )
-  }
+  tinytest::test_package("Rtinycc")
 }
