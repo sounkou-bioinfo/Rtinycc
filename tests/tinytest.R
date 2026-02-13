cfb0c2447d819ebb27839b1fb965de34360e89af
sounkou-bioinfo <sounkoutoure@gmail.com>
2026-02-13 18:18:49 +0100

    tinytest per file

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,4 +1,22 @@
 library(Rtinycc)
+
 if (requireNamespace("tinytest", quietly = TRUE)) {
-  tinytest::test_package("Rtinycc")
+  if (.Platform$OS.type != "windows") {
+    tinytest::test_package("Rtinycc")
+  } else {
+    test_dir <- system.file("tinytest", package = "Rtinycc")
+    files <- sort(list.files(test_dir, pattern = "^test_.*\\.[Rr]$", full.names = TRUE))
+
+    n_fail <- 0L
+    for (f in files) {
+      res <- tinytest::run_test_file(f)
+      n_fail <- n_fail + sum(vapply(res, isFALSE, logical(1)))
+      rm(res)
+      gc()
+    }
+
+    if (n_fail > 0L) {
+      stop(sprintf("%d tinytest failures", n_fail), call. = FALSE)
+    }
+  }
 }
