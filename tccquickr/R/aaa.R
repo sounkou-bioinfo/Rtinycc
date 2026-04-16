c3979793f28c39334ca17468f1a735eb8f3ea742
sounkou-bioinfo <sounkoutoure@gmail.com>
2026-04-16 23:12:48 +0200

    go

--- tccquickr/R/aaa.R
+++
@@ -1,7 +0,0 @@
-# tccquickr - experimental lowering on top of Rtinycc
-# Copyright (C) 2025-2026 Sounkou Mahamane Toure
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-.RtinyccCall <- get(".RtinyccCall", envir = asNamespace("Rtinycc"))
-
-`%||%` <- function(x, y) if (is.null(x)) y else x
--- tccquickr/tests/tinytest.R
+++
@@ -1,4 +0,0 @@
-library(tccquickr)
-if (requireNamespace("tinytest", quietly = TRUE)) {
-  tinytest::test_package("tccquickr")
-}
